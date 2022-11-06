30b976d988695f42fe42dff8cc54f05d9dd4d8b6
Gaurav Rana <smartgauravrana@gmail.com>
2022-11-06 17:52:35 +0530

    oauth initial commit

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from 'src/users/users.module';
+import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { AuthController } from './auth.controller';
+import { GoogleOauthStrategy } from './google-oauth.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [AuthService, GoogleOauthStrategy, JwtStrategy],
+  controllers: [AuthController],
+})
+export class AuthModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { botHelper } from './bot/botHelper';
 import { loginTradingView } from './common/utils/tradingViewHelper';
+var cookieParser = require('cookie-parser');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.use(cookieParser());
+  app.enableCors();
   await app.listen(3000);
 
   // init telegram bot
